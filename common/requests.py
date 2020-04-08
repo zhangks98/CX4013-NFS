814f35fa7afaff4781a6ee288d0ff0ce463bde0e
Kaishuo Zhang <6542131+zhangks98@users.noreply.github.com>
2020-04-08 22:25:32 +0800

    Added value serializer.

--- common/requests.py
+++ common/requests.py
@@ -1,9 +1,10 @@
 from enum import Enum, unique
-
-# Python's Enum starts from 1 to prevent being interpreted as False.
-# Therefore, EMPTY request is ignored.
+from serialize import ByteBuffer, BUF_SIZE
+from values import Value
+from typing import List
 @unique
 class RequestName(Enum):
+    EMPTY = (0, 0)
     READ = (1, 3)
     WRITE = (2, 4)
     GET_ATTR = (3, 1)
@@ -12,15 +13,40 @@ class RequestName(Enum):
     REGISTER = (6, 2)
     FILE_UPDATED = (7, 2)
 
-    def __init__(self, ordinal: int, numParams: int):
+    def __init__(self, ordinal: int, num_params: int):
         self.ordinal = ordinal
-        self.numParams = numParams
+        self.num_params = num_params
 
 class Request():
     def __init__(self, id: int, name: RequestName):
         self.id = id
         self.name = name
-        self.numParams = name.numParams
-        self.params = []
-        
+        self.params: List[Value] = []
+    
+    def get_id(self) -> int:
+        return self.id
 
+    def get_name(self) -> RequestName:
+        return self.name
+    
+    def add_param(self, val):
+        if (len(self.params) >= self.name.num_params):
+            raise ValueError('Parameter length exceeds {}'.format(self.name.num_params))
+        self.params.append(val)
+    
+    def get_param(self, pos) -> Value:
+        return self.params[pos]
+    
+    def set_param(self, pos, val):
+        self.params[pos] = val
+    
+    def to_bytes(self) -> bytes:
+        if (len(self.params) != self.name.num_params):
+            raise ValueError("Unable to serialize request {}: wrong number of parameters. Expected: {}, Actual: {}.".format(self.name, self.name.num_params, len(self.params)))
+        payload = ByteBuffer.allocate(BUF_SIZE)
+        payload.put_int(self.id)\
+            .put_int(self.name.ordinal)\
+                .put_int(self.name.num_params)
+        for val in self.params:
+            payload.put_bytes(val.to_bytes())
+        return payload.to_bytes()
---
+++ common/values.py
@@ -0,0 +1,74 @@
+from enum import Enum, unique
+from serialize import INTEGER_BYTES, LONG_BYTES, ByteBuffer
+from struct import pack
+
+@unique
+class ValueType(Enum):
+    STRING = 0
+    BYTES = 1
+    INT32 = 2
+    INT64 = 3
+
+class Value:
+    def __init__(self, val):
+        self.val = val
+
+    def get_val(self):
+        return self.val
+    
+    def to_bytes(self) -> bytes:
+        raise NotImplementedError
+    
+    @staticmethod
+    def from_bytes(buffer: ByteBuffer) -> 'Value':
+        value_type = buffer.get_int()
+        if value_type == ValueType.STRING.value:
+            return Str.from_bytes(buffer)
+        elif value_type == ValueType.BYTES.value:
+            return Bytes.from_bytes(buffer)
+        elif value_type == ValueType.INT32.value:
+            return Int32.from_bytes(buffer)
+        elif value_type == ValueType.INT64.value:
+            return Int64.from_bytes(buffer)
+        else:
+            raise ValueError("No such value type: {}".format(value_type))
+
+class Str(Value):
+    @classmethod
+    def from_bytes(cls, buffer: ByteBuffer) -> Str:
+        size = buffer.get_int()
+        return cls(buffer.get_bytes(size).decode('utf-8'))
+
+    def to_bytes(self) -> bytes:
+        encoded = self.val.encode('utf-8')
+        size = len(encoded)
+        return pack('>ii{}s'.format(len(self.val)),
+         ValueType.STRING.value, size, encoded)
+
+class Bytes(Value):
+    @classmethod
+    def from_bytes(cls, buffer: ByteBuffer) -> Bytes:
+        size = buffer.get_int()
+        return cls(buffer.get_bytes(size))
+    
+    def to_bytes(self) -> bytes:
+        size = len(self.val)
+        return pack('>ii{}s'.format(size),
+         ValueType.BYTES.value, size, self.val)
+
+class Int32(Value):
+    @classmethod
+    def from_bytes(cls, buffer: ByteBuffer) -> Int32:
+        return cls(buffer.get_int())
+    
+    def to_bytes(self) -> bytes:
+        return pack('>ii', ValueType.INT32.value, self.val)
+
+class Int64(Value):
+    @classmethod
+    def from_bytes(cls, buffer: ByteBuffer):
+        return cls(buffer.get_long())
+    
+    def to_bytes(self):
+        return pack('>il', ValueType.INT64.value, self.val)
+
