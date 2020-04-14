7831717e8af804a6bfee9992d40965521d09906d
Ming <liumcse@gmail.com>
2020-04-14 17:14:54 +0800

    Change second to millisecond and add directory check

--- python/tests/servicer_test.py
+++ python/tests/servicer_test.py
@@ -5,11 +5,13 @@ from pyfakefs.fake_filesystem import FakeFilesystem
 
 from nfs.common.requests import (EmptyRequest, GetAttrRequest, ReadRequest,
                                  TouchRequest, WriteRequest)
+from nfs.common.exceptions import BadRequestError
 from nfs.common.values import Bytes, Int32, Str
 from nfs.server.servicer import ALOServicer, AMOServicer
 
 mock_socket = mock.Mock()
 mock_socket.recv.return_value = []
+addr = "localhost"
 
 
 def get_memory_addr(var):
@@ -23,11 +25,10 @@ class TestALOServier:
     def setup_method(self):
         self.servicer = ALOServicer(".", mock_socket)
 
-    def test_handle_get_Attr(self, fs: FakeFilesystem):
+    def test_handle_get_attr(self, fs: FakeFilesystem):
         fs.create_file('test.txt', contents='test')
         req = GetAttrRequest(0)
         req.add_param(Str('test.txt'))  # Path
-        addr = "localhost"
         val = self.servicer.handle(req, addr)
         mtime = val[0].get_val()
         atime = val[1].get_val()
@@ -38,7 +39,6 @@ class TestALOServier:
         req = TouchRequest(0)
         assert fs.exists('test.txt') is False
         req.add_param(Str('test.txt'))  # Path
-        addr = "localhost"
         self.servicer.handle(req, addr)
         assert fs.exists('test.txt') is True
         atime_old = fs.stat('test.txt').st_atime
@@ -52,23 +52,31 @@ class TestALOServier:
         req.add_param(Int32(2))  # Offset
         req.add_param(Str('test.txt'))  # Path
         req.add_param(Bytes(b'INSERT'))
-        addr = "localhost"
         self.servicer.handle(req, addr)
         with open('test.txt', 'rb') as f:
             content = f.read()
             assert content == b'teINSERTst'
+        fs.create_dir('text.txt')
+        self.servicer.handle(req, addr)
+
+    def test_handle_write_dir(self, fs: FakeFilesystem):
+        fs.create_dir('dir')
+        req = WriteRequest(0)
+        req.add_param(Int32(2))  # Offset
+        req.add_param(Str('dir'))  # Path
+        req.add_param(Bytes(b'INSERT'))
+        with pytest.raises(BadRequestError):
+            self.servicer.handle(req, addr)
 
     def test_handle_read(self, fs: FakeFilesystem):
         fs.create_file('test.txt', contents='test')
         req = ReadRequest(0)
         req.add_param(Str("test.txt"))  # Path
-        addr = "localhost"
         val = self.servicer.handle(req, addr)
         assert val[0].get_val() == b'test'
 
     def test_duplicate_request(self):
         req = EmptyRequest(0)
-        addr = "localhost"
         val_a = self.servicer.handle(req, addr)
         val_b = self.servicer.handle(req, addr)
         assert get_memory_addr(val_a) != get_memory_addr(val_b)
@@ -80,7 +88,6 @@ class TestAMOServicer:
 
     def test_duplicate_request(self):
         req = EmptyRequest(0)
-        addr = "localhost"
         assert self.servicer._is_duplicate_request(req, addr) is None
         val_a = self.servicer.handle(req, addr)
         assert self.servicer._is_duplicate_request(req, addr) is not None
